30bdffcdcd5db18f38e130fbc3d94f149fe44ebe
Liteskarr <398501z3@gmail.com>
2022-09-18 15:14:48 +0300

    fucking big update

---
+++ plugins/import_auto_table.py
@@ -0,0 +1,28 @@
+import importlib
+import pathlib
+from typing import Optional
+
+from pandas import DataFrame
+
+from api import Plugin
+from src.api import PluginImport
+
+
+class ImportPandasLikeTable(PluginImport):
+    def __init__(self):
+        super().__init__(
+            "Таблицу",
+            "Загружает таблицу из файла произвольного расширения.",
+            supported_formats=['*.csv', '*.xls', '*.xlsx', '*.json', '*.xml']
+        )
+
+    def import_from(self, file_path: str, parameters: Plugin.OptionsValues) -> Optional[DataFrame]:
+        end = file_path.split('.')[-1]
+        if f'read_{end}' in (d := importlib.import_module('pandas').__dict__):
+            return d[f'read_{end}'](file_path)
+        else:
+            return None
+
+
+__import_plugins__ = [ImportPandasLikeTable]
+__visual_plugins__ = []
