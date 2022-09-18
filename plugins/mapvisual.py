7112d6b007a5c0a3e080abe7003cdc2a96e3aa1b
Daniil Smirnov <79110269800@yandex.ru>
2022-09-18 06:43:44 +0300

    сделал карту

---
+++ plugins/mapvisual.py
@@ -0,0 +1,46 @@
+import xml.etree.ElementTree
+import pandas as pd
+import pycountry
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtSvg import QSvgWidget
+from pandas import DataFrame
+
+from src.api import PluginQtVisualize, Plugin, VisualizeType
+
+class MapView(PluginQtVisualize):
+    def __init__(self):
+        super().__init__("Map View", "No description", VisualizeType.Map)
+    
+    @staticmethod
+    def paintCountry(id, color, svg_):
+        elm = svg_.find(f'''.//*[@id='{id}']''' )
+        elm.attrib['fill'] = f'{color}'
+        elm.attrib['style'] = ''
+
+    @staticmethod
+    def paintCountriesOneColor(sales, color, svg_):
+        mn = float(sales.iat[1, 1])
+        mx = float(sales.iat[1, 1])
+        for rx in range(1, sales.shape[0]):
+            mx = max(mx, float(sales.iat[rx, 1]))
+            mn = min(mn, float(sales.iat[rx, 1]))
+        raz = mx-mn
+        for rx in range(1, sales.shape[0]):
+                try:
+                    countri = pycountry.countries.get(name = sales.iat[rx, 0])
+                    MapView.paintCountry(countri.alpha_2, color.darker(int((float(sales.iat[rx, 1])-mn)/raz*200)).name(), svg_)
+                except:
+                    pass
+
+    def visualize(self, data: DataFrame, parameters: Plugin.OptionsValues) -> QWidget:
+        et = xml.etree.ElementTree.parse('res\world.svg')
+        MapView.paintCountriesOneColor(data, QColor(255, 0, 0), et)
+        et.write('res\\nworld.svg')
+        svg = QSvgWidget('res\\nworld.svg')
+        return svg
+
+
+__import_plugins__ = []
+__visual_plugins__ = [MapView]
