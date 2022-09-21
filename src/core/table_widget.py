04a6465aadb203acb3e039d302f35cbd69da1c24
NamorNiradnug <roma57linux@gmail.com>
2022-09-22 02:58:34 +0300

    Delete src/core directory

--- src/core/table_widget.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-
-
-class TableWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-
-    def _configure_ui(self):
-        pass
--- src/core/widgets_container.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QHBoxLayout
-
-
-class WidgetsContainer(QHBoxLayout):
-    def swapWidgets(self, i: int, j: int):
-        pass
